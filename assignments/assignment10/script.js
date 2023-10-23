dc15b01c28562bffc862ce8f1f30c1fcd668f3c2
Luke Lane <lukeag@email.sc.edu>
2023-10-23 15:56:29 -0400

    Forgot to push from last week, working on part 4 now

--- assignments/assignment10/script.js
+++ assignments/assignment10/script.js
@@ -1,5 +1,36 @@
+class Toy{
+    constructor(name, price, age, rating){
+        this.name = name;
+        this.price = price;
+        this.age = age;
+        this.rating = rating;
+    }
+    
+    getDetails(toy){
 
+    }
+
+    getToyItem(){
+
+    }
+
+}
+
+const createToys = () => {
+    const table = document.getElementById("toys-table");
+    const toys = [];
+    toys.push(new Toy("Avacado", 15, "5+", 1));
+    toys.push(new Toy("Baby Yoda", 45, "7+", 5));
+    toys.push(new Toy("Large Bear", 75, "9+", 5));
+    toys.push(new Toy("Buffalo", 35, "6+", 4));
+    toys.push(new Toy("Teddy Bear", 20, "3+", 3));
+    toys.push(new Toy("Turtle", 18, "3+", 2));
+
+    toys.forEach(toy => {
+        table.append(toy.tableRow);
+    });
+};
 
 window.onload = () => {
-
+    createToys();
 }
