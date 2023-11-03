a54a37a09e671e15928f3528bc3ee47c4babee81
Luke Lane <lukeag@email.sc.edu>
2023-11-03 18:23:30 -0400

    forgot something

--- projects/part5/store.js
+++ projects/part5/store.js
@@ -1,6 +1,6 @@
 const getStore = async () => {
 
-    const url = await fetch("store-items.json");
+    const url = "https://lukelane7.github.io/projects/part5/store-items.json";
 
     try {
       const link = await fetch(url); 
@@ -14,10 +14,45 @@ const getStore = async () => {
   const showStore = async () => {
     let store = await getStore(); 
     let storeSection = document.getElementById("main-content");
+
+    store.forEach(item => {
+        
+        storeSection.append(getStoreItem(item))
+      });    
   };
   
-  const getStoreItem = (movie) => {
+  const getStoreItem = (item) => {
+
     
+    let section = document.createElement("div");
+    section.id = "store-listing";
+    section.classList.add("col1of2");
+
+    //Tried to get 4 of them into a flex container so they would line up next to eachother and have another row underneath the 4
+    //but i couldnt correctly loop 4 of them together in this or the other function
+
+    const h8 = document.createElement("h8");
+    h8.innerText = item.name; 
+    section.append(h8);
+
+    const img = document.createElement("img"); 
+    const imgCont = document.createElement("div");
+    img.src = item.img;
+    img.classList.add("store-img");
+    imgCont.append(img);
+    section.append(imgCont);
+
+    const h6 = document.createElement("h6");
+    h6.innerText = item.price; 
+    section.append(h6);
+
+    const pButton = document.createElement("button");
+    pButton.innerText = "Sign in & Purchase";
+    pButton.id = "store-button";
+    section.append(pButton);
+
+    return section;
+
   };
 
   window.onload = () => showStore(); 
